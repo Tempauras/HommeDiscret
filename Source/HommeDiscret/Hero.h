0ed9855450c3857f350d601734828df00c19d975
zolori <44702305+zolori@users.noreply.github.com>
2022-01-18 18:15:28 +0100

    3C - tuto

--- Source/HommeDiscret/Hero.h
+++ Source/HommeDiscret/Hero.h
@@ -3,12 +3,19 @@
 #pragma once
 
 #include "CoreMinimal.h"
-#include "Components/CapsuleComponent.h"
-#include "GameFramework/Controller.h"
-#include "GameFramework/Character.h"
-#include "GameFramework/CharacterMovementComponent.h"
-#include "GameFramework/SpringArmComponent.h"
+
 #include "Camera/CameraComponent.h"
+#include "Components/CapsuleComponent.h"
+#include "Components/StaticMeshComponent.h"
+#include "Components/InputComponent.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "GameFramework/Controller.h"
+#include "GameFramework/SpringArmComponent.h"
+
+//#include "Blueprint/UserWidget.h"
+
+#include "GameFramework/Character.h"
+
 #include "Hero.generated.h"
 
 UCLASS()
@@ -20,15 +27,18 @@ public:
 	// Sets default values for this character's properties
 	AHero();
 
-protected:
-
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera)
 		USpringArmComponent* CameraBoom;
-
 
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera)
 		UCameraComponent* FollowCamera;
 
+	void MoveForward(float Axis);
+	void MoveRight(float Axis);
+
+	bool bDead;
+
+protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
