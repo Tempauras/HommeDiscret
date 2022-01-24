7e7b3651eae36112b1e11bcc379117dfcad4c688
Temporas <kys76123456@gmail.com>
2022-01-24 12:10:45 +0100

    Revert "Update Socket skeleton"
    This reverts commit f1a38055c30f2a12fe5f855968ddc213e3b585a2.

---
+++ enc_temp_folder/5ebee92a516ef4d54a43e8af4ddeece1/Hero.cpp
@@ -0,0 +1,144 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Hero.h"
+#include "Food.h"
+
+// Sets default values
+AHero::AHero()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	
+	PrimaryActorTick.bCanEverTick = true;
+
+	bUseControllerRotationPitch = false;
+	bUseControllerRotationRoll = false;
+	bUseControllerRotationYaw = false;
+
+	GetCharacterMovement()->bOrientRotationToMovement = true;
+	GetCharacterMovement()->RotationRate = FRotator(0.0f, 540.0f, 0.0f);
+
+	CameraBoom = CreateDefaultSubobject<USpringArmComponent>(TEXT("CameraBoom"));
+	CameraBoom->SetupAttachment(RootComponent);
+	CameraBoom->TargetArmLength = 300.0f;
+	CameraBoom->bUsePawnControlRotation = true;
+
+	FollowCamera = CreateDefaultSubobject<UCameraComponent>(TEXT("FollowCamera"));
+	FollowCamera->SetupAttachment(CameraBoom, USpringArmComponent::SocketName);
+	FollowCamera->bUsePawnControlRotation = false;
+
+	FoodMesh = CreateDefaultSubobject<UStaticMeshComponent>(FName("FoodMesh"));
+	FoodMesh->SetSimulatePhysics(false);
+	FoodMesh->SetCollisionEnabled(ECollisionEnabled::PhysicsOnly);
+
+
+	bDead = false;
+
+	setup_stimulus();
+}
+
+// Called when the game starts or when spawned
+void AHero::BeginPlay()
+{
+	Super::BeginPlay();
+	if (GetMesh()->DoesSocketExist("FoodSocket"))
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Exist!"));
+		FoodMesh->AttachToComponent(GetMesh(), FAttachmentTransformRules::SnapToTargetNotIncludingScale, TEXT("FoodSocket"));
+	}
+}
+
+// Called every frame
+void AHero::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void AHero::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	PlayerInputComponent->BindAxis("Turn", this, &APawn::AddControllerYawInput);
+	PlayerInputComponent->BindAxis("LookUp", this, &APawn::AddControllerPitchInput);
+
+	PlayerInputComponent->BindAction("Jump", IE_Pressed, this, &ACharacter::Jump);
+	PlayerInputComponent->BindAction("Jump", IE_Released, this, &ACharacter::StopJumping);
+
+	PlayerInputComponent->BindAction("Interact", IE_Pressed, this, &AHero::Interact);
+
+	PlayerInputComponent->BindAxis("MoveForward", this, &AHero::MoveForward);
+	PlayerInputComponent->BindAxis("MoveRight", this, &AHero::MoveRight);
+
+	PlayerInputComponent->BindAxis("Zoom", this, &AHero::Zoom);
+}
+
+void AHero::setup_stimulus()
+{
+	stimulus = CreateDefaultSubobject<UAIPerceptionStimuliSourceComponent>(TEXT("stimulus"));
+	stimulus->RegisterForSense(TSubclassOf<UAISense_Sight>());
+	stimulus->RegisterWithPerceptionSystem();
+
+}
+
+void AHero::MoveForward(float Axis)
+{
+	if (!bDead)
+	{
+		const FRotator Rotation = Controller->GetControlRotation();
+		const FRotator YawRotation(0, Rotation.Yaw, 0);
+
+		const FVector Direction = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::X);
+		AddMovementInput(Direction, Axis);
+	}
+}
+
+void AHero::MoveRight(float Axis)
+{
+	if (!bDead)
+	{
+		const FRotator Rotation = Controller->GetControlRotation();
+		const FRotator YawRotation(0, Rotation.Yaw, 0);
+
+		const FVector Direction = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::Y);
+		AddMovementInput(Direction, Axis);
+	}
+}
+
+void AHero::Zoom(float value)
+{
+	if (value)
+	{
+		float temp = CameraBoom->TargetArmLength + (value * -10);
+		if (temp < 810 && temp > 100)
+		{
+			CameraBoom->TargetArmLength = temp;
+		}
+	}
+}
+
+void AHero::Interact()
+{
+	if (!bDead)
+	{
+		TArray<AActor*> OverlappedActors;
+		this->GetOverlappingActors(OverlappedActors);
+		for (AActor* OverlappedActor : OverlappedActors)
+		{
+			AFood* Food = Cast<AFood>(OverlappedActor);
+			if (Food != nullptr)
+			{
+				if (Food->bIsAblePickup)
+				{
+					UE_LOG(LogTemp, Warning, TEXT("Interacting with food!"));
+					FoodMesh->SetStaticMesh(Food->StaticMesh->GetStaticMesh());
+					Food->Destroy();
+				}
+			}
+		}
+		OverlappedActors.Empty();
+	}
+}
+
+
