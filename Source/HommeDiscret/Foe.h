34cf1982eb172194b3a604ad9437d3f14d6124e1
Paul Josserand <paul.josserand.rdl@gmail.com>
2022-01-31 14:11:07 +0100

    Revert "Revert "IA Functions""
    This reverts commit 8395673c94955bd5486811ced982fcec90699ff8.

--- Source/HommeDiscret/Foe.h
+++ Source/HommeDiscret/Foe.h
@@ -8,6 +8,8 @@
 #include "Foe.generated.h"
 
 class AFood;
+class UCapsuleComponent;
+class USphereComponent;
 UCLASS()
 class HOMMEDISCRET_API AFoe : public ACharacter
 {
@@ -22,15 +24,41 @@ protected:
 		AFood* FoodRef;
 
 	UPROPERTY(VisibleAnywhere)
-		bool HaveToDroppedFood;
-	
+		USphereComponent* CollisionSphere;
+
 	UPROPERTY(VisibleAnywhere)
-		bool HoldingFood;
+		UCapsuleComponent* CollisionCylinder;
+
+	UPROPERTY(VisibleAnywhere, Category = "Food")
+		UStaticMeshComponent* FoodMesh;
+
+	UPROPERTY(VisibleAnywhere)
+		float CollisionSphereRadius=100.0f;
+
+	UPROPERTY(VisibleAnywhere)
+		bool HaveToDroppedFood;
+
+	UPROPERTY(VisibleAnywhere)
+		bool AreInteracing;
+
+	UPROPERTY(VisibleAnywhere)
+		bool IsHoldingFood;
 
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
 public:	
+
+	UFUNCTION()
+		void CallbackComponentBeginOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+
+	UFUNCTION()
+		void CallbackComponentEndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
+
+	void SetAreInteracting(bool NewInteract);
+	void PickUpFood();
+	void DropFood();
+
 	// Called every frame
 	bool GetHaveToDroppedFood();
 	bool GetHoldingFood();
