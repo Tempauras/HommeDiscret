9d45c1635b7787c94052a75f1d7fb14507173c45
zolori <44702305+zolori@users.noreply.github.com>
2022-02-07 16:30:22 +0100

    Animation Enemies

--- Source/HommeDiscret/Tools/Animations/Goblin_AnimInstance.cpp
+++ Source/HommeDiscret/Tools/Animations/Goblin_AnimInstance.cpp
@@ -2,23 +2,45 @@
 
 
 #include "Goblin_AnimInstance.h"
+#include "HommeDiscret/IA/Foe.h"
 
 UGoblin_AnimInstance::UGoblin_AnimInstance()
 {
 	Speed = 0;
-	IsMovingG = false;
+	IsMoving = false;
+	IsHolding = false;
+	IsInHand = false;
 }
 
 void UGoblin_AnimInstance::NativeUpdateAnimation(float DeltaSeconds)
 {
 	Super::NativeUpdateAnimation(DeltaSeconds);
-	AActor* OwningActor = GetOwningActor();
+	AFoe* Foe = Cast<AFoe>(GetOwningActor());
+	//AActor* Foe = GetOwningActor();
 
-	if (OwningActor != nullptr)
-		Speed = OwningActor->GetVelocity().Size();
+	if (Foe != nullptr) {
+		Speed = Foe->GetVelocity().Size();
+		IsHolding = Foe->GetHoldingFood();
+	}
 
 	if (Speed > 0)
-		IsMovingG = true;
+		IsMoving = true;
 	else
-		IsMovingG = false;
+		IsMoving = false;
+
+	if (!IsHolding)
+		IsInHand = false;
+}
+
+void UGoblin_AnimInstance::AnimNotify_End(UAnimNotify* Notify)
+{
+	GEngine->AddOnScreenDebugMessage(-1, 5.0f, FColor::Green, TEXT("end"));
+	//MovementPtr->MaxWalkSpeed = Hero->HeroSpeed;
+	IsInHand = true;
+}
+
+void UGoblin_AnimInstance::AnimNotify_Begin(UAnimNotify* Notify)
+{
+	GEngine->AddOnScreenDebugMessage(-1, 5.0f, FColor::Green, TEXT("begin"));
+	//MovementPtr->MaxWalkSpeed = 0;
 }
