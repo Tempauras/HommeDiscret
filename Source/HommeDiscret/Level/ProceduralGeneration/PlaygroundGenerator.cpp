31e2e5307cf3207a0169bc92d355df9b7e0062d3
Temporas <kys76123456@gmail.com>
2022-02-07 17:32:51 +0100

    Add foe spawner to game state

--- Source/HommeDiscret/Level/ProceduralGeneration/PlaygroundGenerator.cpp
+++ Source/HommeDiscret/Level/ProceduralGeneration/PlaygroundGenerator.cpp
@@ -100,6 +100,7 @@ void APlaygroundGenerator::SpawnPlayground()
 		if (GS != nullptr)
 		{
 			GS->FoodSpotList = FoodSpotList;
+			GS->FoeSpawner = FoeSpawnerRoomReferences->FoeSpawner;
 		}
 	}
 }
